27d951ca8307f413fd36cbf50c98a091e94bda0b
Nnamdi <nnamdi.b@gmail.com>
2022-09-20 18:04:12 +0200

    cleaned up code, added fetch, added console log

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import { Box, Button, Container, FormControl, TextField } from '@mui/material';
 import Head from 'next/head';
-import Link from 'next/link';
 import { useRef } from 'react';
 
 function HomePage() {
-  const emailInputRef = useRef();
-  const feedbackInputRef = useRef();
+  const emailInputRef = useRef(null);
+  const feedbackInputRef = useRef(null);
 
   function submitFormHandler(event) {
     event.preventDefault();
@@ -13,7 +12,14 @@ function HomePage() {
     const enteredEmail = emailInputRef.current.value;
     const enteredFeedback = feedbackInputRef.current.value;
 
-    fetch(); // { email: "test@test.com", text: "Some feedback text" }
+    const reqBody = { email: enteredEmail, text: enteredFeedback };
+    console.log('reqBody: ' + reqBody);
+
+    fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify(reqBody),
+      headers: { 'Content-Type': 'application/json' },
+    }).then((response) => response.json().then((data) => console.log(data)));
   }
 
   return (
@@ -26,24 +32,30 @@ function HomePage() {
         <Box>
           <h1>The Home Page</h1>
         </Box>
-        <FormControl onSubmit={submitFormHandler}>
-          <TextField
-            id='my-email'
-            label='Email'
-            variant='outlined'
-            ref={emailInputRef}
-          />
-          <TextField
-            sx={{ mb: 2 }}
-            id='my-feedback'
-            label='Your feedback'
-            multiline
-            rows={4}
-            variant='standard'
-            ref={feedbackInputRef}
-          />
-          <Button variant='standard'>Send feedback</Button>
-        </FormControl>
+        <form onSubmit={submitFormHandler}>
+          <FormControl>
+            <TextField
+              sx={{ mb: 2 }}
+              id='my-email'
+              label='Email'
+              required={true}
+              variant='outlined'
+              ref={emailInputRef}
+            />
+            <TextField
+              sx={{ mb: 2 }}
+              id='my-feedback'
+              label='Your feedback'
+              multiline
+              rows={4}
+              variant='standard'
+              ref={feedbackInputRef}
+            />
+            <Button variant='standard' type='submit'>
+              Send feedback
+            </Button>
+          </FormControl>
+        </form>
       </Container>
     </div>
   );
