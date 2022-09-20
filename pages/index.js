dc5469cabeef21d4ec9158bfb8de9236def47e8c
Nnamdi <nnamdi.b@gmail.com>
2022-09-20 16:15:47 +0200

    added some server side code to the api route

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
-import {
-  Box,
-  Button,
-  Container,
-  FormControl,
-  FormHelperText,
-  Input,
-  InputLabel,
-  TextField,
-} from '@mui/material';
+import { Box, Button, Container, FormControl, TextField } from '@mui/material';
 import Head from 'next/head';
 import Link from 'next/link';
+import { useRef } from 'react';
 
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    fetch(); // { email: "test@test.com", text: "Some feedback text" }
+  }
+
   return (
     <div>
       <Head>
@@ -22,8 +26,13 @@ function HomePage() {
         <Box>
           <h1>The Home Page</h1>
         </Box>
-        <FormControl>
-          <TextField id='my-email' label='Email' variant='outlined' />
+        <FormControl onSubmit={submitFormHandler}>
+          <TextField
+            id='my-email'
+            label='Email'
+            variant='outlined'
+            ref={emailInputRef}
+          />
           <TextField
             sx={{ mb: 2 }}
             id='my-feedback'
@@ -31,6 +40,7 @@ function HomePage() {
             multiline
             rows={4}
             variant='standard'
+            ref={feedbackInputRef}
           />
           <Button variant='standard'>Send feedback</Button>
         </FormControl>
